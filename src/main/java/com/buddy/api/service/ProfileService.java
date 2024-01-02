ddc56b364ae6342aab8f1679d361653d9dd4c62f
usnim <sms897@naver.com>
2024-01-02 21:52:41 +0900

    [refactor] profileService 프로필 추가 기능 중 입력값의 자격증이 존재하는 자격증인지 확인하는 기능

--- src/main/java/com/buddy/api/service/ProfileService.java
+++ src/main/java/com/buddy/api/service/ProfileService.java
@@ -1,7 +1,9 @@
 package com.buddy.api.service;
 
 import com.buddy.api.controller.profile.request.ProfileRequest;
+import com.buddy.api.domain.LicenseLevel;
 import com.buddy.api.domain.Profile;
+import com.buddy.api.exception.LicenseNotFoundException;
 import com.buddy.api.exception.MemberNotFoundException;
 import com.buddy.api.repository.member.MemberRepository;
 import com.buddy.api.repository.profile.ProfileRepository;
@@ -21,7 +23,14 @@ public class ProfileService {
     }
     @Transactional
     public Profile create(String memberId, ProfileRequest request) {
+        validateProfile(request);
         return profileRepository.save(Profile.of(request.getLevel(), request.getGender(), request.getRegion(),
                 memberRepository.findById(memberId).orElseThrow(() -> new MemberNotFoundException(memberId))));
+    }
+
+    private void validateProfile(ProfileRequest request) {
+        if (!LicenseLevel.isExist(request.getLevel())) {
+            throw new LicenseNotFoundException();
+        }
     }
 }
