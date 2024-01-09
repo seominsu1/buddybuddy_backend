eca918cebfa558741d8ce99a365938605a837866
usnim <sms897@naver.com>
2024-01-09 17:21:52 +0900

    [refactor] 불필요 import 제거

--- src/main/java/com/buddy/api/config/SecurityConfig.java
+++ src/main/java/com/buddy/api/config/SecurityConfig.java
@@ -9,8 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,9 +18,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
--- src/main/java/com/buddy/api/controller/post/PostApiSpec.java
+++ src/main/java/com/buddy/api/controller/post/PostApiSpec.java
@@ -1,6 +1,5 @@
 package com.buddy.api.controller.post;
 
-import com.buddy.api.controller.pool.request.PoolRequest;
 import com.buddy.api.controller.post.request.PostRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
